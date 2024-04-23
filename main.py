258732384e83b6c3e030daef65f2ca13aaed3e44
kkhhy6 <84577612+kkhhy6@users.noreply.github.com>
2024-04-23 11:53:14 +0200

    added Choose_color function

---
+++ main.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from tkinter import  colorchooser
+from tkinter import ttk
+color = None
+def Choose_color():
+    global color
+    color = colorchooser.askcolor()
+    print(color)
+    #TODO check if tk gets whole color to paint or is just part of it needed
+
+root = Tk()
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+ttk.Button(frm, text="Choose Color", command=Choose_color).grid(column=0, row=0)
+root.mainloop()
