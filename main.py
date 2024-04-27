2143132008ef0d7e22a26484092ae6d77c8d1b3a
KrzysztofTwardowski <u22eKrzTwa@liceum.p.lodz.pl>
2024-04-27 10:37:33 +0100

    select function now draws oval instead of line

--- main.py
+++ main.py
@@ -1,33 +1,42 @@
 from tkinter import *
 from tkinter import colorchooser
 from tkinter import ttk
-color = "black"
+brush_color = "black"
+brush_size = 10
+
 
 def Choose_color():
     #asking user to choose color using deafult color choosing dialog window
-    global color
-    color = colorchooser.askcolor()[1]#getting color value, "[1]" gets only hex skipping rgb value
-    print(color) #debug log with hexadecimal value
+    global brush_color
+    brush_color = colorchooser.askcolor()[1]#getting color value, "[1]" gets only hex skipping rgb value
+    print(brush_color) #debug log with hexadecimal value
 
+
+def Change_brush_width():
+    global brush_size
+    brush_size += 1
 
 previous = (0,0)#creating variable "previous" to check last mouse location
 def select(event): #funcion to draw indyvidual pixels
     global previous
     widget = event.widget
-    canvas.create_line(event.x, event.y, event.x + 1, event.y)#creating line that actually is a dot
+    top_left = (event.x - brush_size/2, event.y - brush_size/2)
+    bottom_right = (event.x + brush_size/2, event.y + brush_size/2)
+    canvas.create_oval(*top_left, *bottom_right, outline=brush_color, fill=brush_color)
+    # canvas.create_line(event.x, event.y, event.x + 1, event.y, width=brush_size)#creating line that actually is a dot
     previous = (event.x, event.y)
 
 
 def drag(event): #function to draw lines when dragging mouse
     global previous
-    canvas.create_line(previous[0], previous[1], event.x + 1, event.y, fill=color) #creating line (this time it is a line) when dragging mouse
+    canvas.create_line(previous[0], previous[1], event.x, event.y, fill=brush_color, width=brush_size) #creating line (this time it is a line) when dragging mouse
     widget = event.widget
     previous = (event.x, event.y)#changng previous so it will be accurate next time
 
 do_nothing = lambda: None
 
 root = Tk()
-root.minsize(300,500) # setting minimal windows size STC
+root.minsize(400,500) # setting minimal windows size STC
 root.title("BAINT") # setting window title
 
 application = ttk.Frame(root, padding=10) # creating frame for root window
@@ -37,6 +46,7 @@ application.grid()#adding grid to frame created 2 lines ago
 # Menubar setup
 menubar = Menu(root)
 root.config(menu=menubar)
+
 
 menu_tree = {
     "file": [
@@ -64,11 +74,8 @@ for menu_name in menu_tree:
     )
 
 
-
-# ttk.Button(application, text="Choose Color", command=Choose_color).grid(column=0, row=0)#added button to use function 'Choose_color', for more reference check function description
-
 #!IMPORTANT, canvas use real pixels, only way to make pixel on bitmap bigger on screen is resizing photo, possible solution will be included in 'CanvasIssue.txt'(file not added yet, will be created soon)
-canvas = Canvas(root, cursor="circle", width=100, height=100, bg="#ffffff")#creating canva to draw on, setting circle cursor to make drawing experience better
+canvas = Canvas(root, cursor="circle", width=300, height=300, bg="#ffffff")#creating canva to draw on, setting circle cursor to make drawing experience better
 canvas.grid()#creating grid for canvas
 #next line is made for debugging window size, keep commented if unnecessary
 #line = canvas.create_line(0,0,100,100)
