3bfff9d03b42ba48875e28e5477bb48e66c4fa8d
KrzysztofTwardowski <u22eKrzTwa@liceum.p.lodz.pl>
2024-04-27 10:17:59 +0100

    Added menubar

--- main.py
+++ main.py
@@ -1,35 +1,74 @@
 from tkinter import *
-from tkinter import  colorchooser
+from tkinter import colorchooser
 from tkinter import ttk
 color = "black"
+
 def Choose_color():
     #asking user to choose color using deafult color choosing dialog window
     global color
     color = colorchooser.askcolor()[1]#getting color value, "[1]" gets only hex skipping rgb value
     print(color) #debug log with hexadecimal value
 
-previous = (0,0)#creating variable "previous" to check last mouse location
 
+previous = (0,0)#creating variable "previous" to check last mouse location
 def select(event): #funcion to draw indyvidual pixels
     global previous
     widget = event.widget
     canvas.create_line(event.x, event.y, event.x + 1, event.y)#creating line that actually is a dot
     previous = (event.x, event.y)
+
+
 def drag(event): #function to draw lines when dragging mouse
     global previous
     canvas.create_line(previous[0], previous[1], event.x + 1, event.y, fill=color) #creating line (this time it is a line) when dragging mouse
     widget = event.widget
     previous = (event.x, event.y)#changng previous so it will be accurate next time
 
+do_nothing = lambda: None
+
 root = Tk()
-root.minsize(300,500) #setting minimal windows size STC
-frm = ttk.Frame(root, padding=10)#creating frame for root window
-root.title("BAINT")#setting window title
-frm.grid()#adding grid to frame created 2 lines ago
-ttk.Button(frm, text="Choose Color", command=Choose_color).grid(column=0, row=0)#added button to use function 'Choose_color', for more reference check function description
+root.minsize(300,500) # setting minimal windows size STC
+root.title("BAINT") # setting window title
+
+application = ttk.Frame(root, padding=10) # creating frame for root window
+application.grid()#adding grid to frame created 2 lines ago
+
+
+# Menubar setup
+menubar = Menu(root)
+root.config(menu=menubar)
+
+menu_tree = {
+    "file": [
+        ["new", do_nothing],
+        ["import", do_nothing],
+        ["export", do_nothing],
+        ["exit", do_nothing],
+    ],
+    "brush": [
+        ["color", Choose_color],
+        ["size", do_nothing],
+    ],
+}
+
+for menu_name in menu_tree:
+    new_menu = Menu(menubar)
+    for item in menu_tree[menu_name]:
+        new_menu.add_command(
+            label=item[0],
+            command=item[1]
+        )
+    menubar.add_cascade(
+        label=menu_name,
+        menu=new_menu
+    )
+
+
+
+# ttk.Button(application, text="Choose Color", command=Choose_color).grid(column=0, row=0)#added button to use function 'Choose_color', for more reference check function description
 
 #!IMPORTANT, canvas use real pixels, only way to make pixel on bitmap bigger on screen is resizing photo, possible solution will be included in 'CanvasIssue.txt'(file not added yet, will be created soon)
-canvas = Canvas(root, cursor="circle", widt=100, height=100)#creating canva to draw on, setting circle cursor to make drawing experience better
+canvas = Canvas(root, cursor="circle", width=100, height=100, bg="#ffffff")#creating canva to draw on, setting circle cursor to make drawing experience better
 canvas.grid()#creating grid for canvas
 #next line is made for debugging window size, keep commented if unnecessary
 #line = canvas.create_line(0,0,100,100)
